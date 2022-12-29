17738b066da94518c6988de708c9221b74c11260
Jenil666 <jenilchandarana33@gmail.com>
2022-12-29 21:56:08 +0530

    Initial commit

---
+++ lib/code.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/cupertino.dart';
+
+class code extends StatefulWidget {
+  const code({Key? key}) : super(key: key);
+
+  @override
+  State<code> createState() => _codeState();
+}
+
+class _codeState extends State<code> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
---
+++ lib/main.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:gstcalculator_2/code.dart';
+
+void main()
+{
+  runApp(
+    MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: code(),
+    ),
+  );
+}
